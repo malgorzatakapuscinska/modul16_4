c6729232e56958047232fd1f5b654137da1c83da
Malgorzata Kapuscinska <m_kapuscinska1@o2.pl>
2018-04-05 23:26:28 +0200

    Get render bug

---
+++ src/containers/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            data: []
+        };
+    }
+    addTodo(val){
+        const todo = {
+            text: val,
+            id: uuid.v4(),
+        };
+        const data = [...this.state.data, todo];
+        this.setState({data});
+    }
+}
+
+export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,10 @@
 const path = require('path');
 
 module.exports = {
-	entry: './src/app.js',
+	entry: './src/index.js',
 		output: {
 			path: path.resolve(__dirname, 'build'),
-			filename: 'app.boundle.js'
+			filename: 'index.boundle.js'
 	},
 	module: {
 		rules: [ 
