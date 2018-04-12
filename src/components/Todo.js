5742eb38a8528507c26b6d2c7fe8c53f80e897f1
Malgorzata Kapuscinska <m_kapuscinska1@o2.pl>
2018-04-12 15:06:02 +0200

    Remove console logs and center app in viev port

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -2,10 +2,6 @@ import React from 'react';
 import style from './Todo.css';
 
 const Todo = (props) => {
-	console.log(props);
-	console.log(props.todo.text);
-	console.log(props.remove);
-	console.log(props.todo.id);
 	return (
 		<li className={style.Todo}>
 			<a href={'#'}>
